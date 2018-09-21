59d60bfc82a753d3ce64ddb6762b03fadb14acd0
JadenYangCA <jaden.j.yang@gmail.com>
2018-09-21 13:42:35 -0600

    delete the unused code

--- src/main/java/github/com/jadenyangca/analyzer/utils/PropertiesUtil.java
+++ src/main/java/github/com/jadenyangca/analyzer/utils/PropertiesUtil.java
@@ -1,4 +1,5 @@
 package github.com.jadenyangca.analyzer.utils;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.Enumeration;
@@ -11,20 +12,26 @@ import org.apache.logging.log4j.Logger;
 
 public class PropertiesUtil {
 	private static Logger logger = LogManager.getLogger();
+	
+	//configuration file name
 	private static final String configFileName = "config.properties";
+	
+	//configuration file path
 	private final String configFilePath = PropertiesUtil.class.getClassLoader().getResource(configFileName).getFile();
-	//对应于属性文件的文件对象变量
+	
+	//configuration file object
 	private final File configFile = new File(configFilePath);
-	//属性文件的最后修改日期
+	
+	// last Modified Time of configuration file 
 	private long lastModifiedTime = 0;
-	//属性文件所对应的属性对象变量
+	
 	private Properties props = null;
-	
-	//本类可能存在的唯一的唯一实例
+
+	// singleton instance 
 	private static PropertiesUtil instance = new PropertiesUtil();
-	
+
 	/**
-	 * 私有构造方法，用以保证外界无法直接实例化
+	 * private constructor
 	 */
 	private PropertiesUtil() {
 		props = new Properties();
@@ -35,41 +42,61 @@ public class PropertiesUtil {
 		}
 	}
 
+	/**
+	 * get singleton instance
+	 * @return PropertiesUtil instance
+	 */
 	public static synchronized PropertiesUtil getInstance() {
 //		logger.info("user.dir1:" + System.getProperty("user.dir"));
 		return instance;
 	}
 
-	public String getValueNewest(String keyName,String defaultVal) {
+	/**
+	 * get property value
+	 * @param keyName property name
+	 * @return property value
+	 */
+	public String getValue(String keyName) {
+		return props.getProperty(keyName);
+	}
+	
+	/**
+	 * get the newest value by name, reload the configuration if it is modified
+	 * @param keyName property key name
+	 * @param defaultVal property value if it doesn't exist
+	 * @return property value
+	 */
+	public String getValueNewest(String keyName, String defaultVal) {
 		long newTime = configFile.lastModified();
-		//检查属性文件是否被其他的程序修改过，如果是，重读此配置文件
+		// check whether configuration file is modified, if it is, reload it
 		if (newTime == 0) {
-			//属性文件不存在
+			// configuration file is not existent
 			if (lastModifiedTime == 0) {
 				logger.error(configFilePath + " file does not exist !");
 			} else {
 				logger.error(configFilePath + " file was deleted !");
 			}
-			
+
 			return defaultVal;
 		} else if (newTime > lastModifiedTime) {
-			//属性文件被修改过,重新加载配置文件
+			// reload configuration file
 			props.clear();
 			try {
 				props.load(new FileInputStream(configFile));
-			}  catch (Exception e) {
+			} catch (Exception e) {
 				logger.error(e);
 			}
 		}
 		lastModifiedTime = newTime;
 		return getValue(keyName, defaultVal);
 	}
-	
 
-	public String getValue(String keyName) {
-		return props.getProperty(keyName);
-	}
-	
+	/**
+	 * get the value that may be out of date 
+	 * @param keyName property key name
+	 * @param defaultVal property value if it doesn't exist
+	 * @return property value
+	 */
 	public String getValue(String keyName, String defaultValue) {
 		String val = props.getProperty(keyName);
 		if (val == null) {
@@ -78,40 +105,39 @@ public class PropertiesUtil {
 			return val;
 		}
 	}
-	
 
-	public void setValue(String itemName,String value) {
-		props.setProperty(itemName, value);
-		return;
-	}
-	
+	/*
+	 * get all keys and values from configuration
+	 */
 	public Map<String, String> getAllKeyAndValue() {
 		Map<String, String> map = new HashMap<String, String>();
-		//遍历
 		Enumeration<?> en = props.propertyNames();
 		while (en.hasMoreElements()) {
-			String key = en.nextElement().toString();//key值
+			String key = en.nextElement().toString();
 			String value = props.getProperty(key);
 			map.put(key, value);
 		}
 		return map;
 	}
 	
-	public Map<String, String> getAllKeyAndValue2() {
-		Map<String, String> map = new HashMap<String, String>();
-		props.list(System.out);
-		Object[] objs = props.keySet().toArray();
-		for (int i = 0; i < objs.length; i++) {
-			map.put(objs[i].toString(), props.get(objs[i]).toString());
-		}
-		return map;
-	}
+	/**
+	public void setValue(String itemName, String value) {
+		props.setProperty(itemName, value);
+		return;
+	}*/
 	
-	public static void main(String[] args) {
-		
-		//1.获得java虚拟机的参数
-		Properties pps = System.getProperties();
-		pps.list(System.out);
-		
-	}
+//	public Map<String, String> getAllKeyAndValue2() {
+//		Map<String, String> map = new HashMap<String, String>();
+//		props.list(System.out);
+//		Object[] objs = props.keySet().toArray();
+//		for (int i = 0; i < objs.length; i++) {
+//			map.put(objs[i].toString(), props.get(objs[i]).toString());
+//		}
+//		return map;
+//	}
+
+//	public static void main(String[] args) {
+//		Properties pps = System.getProperties();
+//		pps.list(System.out);
+//	}
 }
